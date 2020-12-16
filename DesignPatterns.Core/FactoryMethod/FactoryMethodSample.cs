62462aec5c2470316446bf86de2c18830a6d71eb
Kuzmin Nick <kuzmin.nick@rambler.ru>
2020-12-16 21:24:31 +0300

    Add FactoryMethod

---
+++ DesignPatterns.Core/FactoryMethod/FactoryMethodSample.cs
@@ -0,0 +1,58 @@
+﻿namespace DesignPatterns.Core.FactoryMethod
+{
+    abstract class Exporter
+    {
+        public abstract void Export();
+    }
+
+    class XmlExporter : Exporter
+    {
+        public override void Export()
+        { }
+    }
+
+    class PdfExporter : Exporter
+    {
+        public override void Export()
+        { }
+    }
+
+    abstract class ExporterCreator
+    {
+        public abstract Exporter Create();
+    }
+
+    class PdfExporterCreator : ExporterCreator
+    {
+        public override Exporter Create() { return new PdfExporter(); }
+    }
+
+    class XmlExporterCreator : ExporterCreator
+    {
+        public override Exporter Create() { return new XmlExporter(); }
+    }
+
+    class Client
+    {
+        public void Run(string type)
+        {
+            ExporterCreator creator;
+            if (type == "Pdf")
+            {
+                creator = new PdfExporterCreator();
+            }
+            else if (type == "Xml")
+            {
+                creator = new XmlExporterCreator();
+            }
+
+            Export(creator);
+        }
+
+        private void Export(ExporterCreator creator)
+        {
+            var exporter = creator.Create();
+            exporter.Export();
+        }
+    }
+}
