b73ac87ef1aee35bd6b75de14ded28792ed70335
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2022-01-09 22:15:29 +0300

    Flyweight

---
+++ DesignPatterns.Core/StructuralPatterns/Flyweight/Flyweight.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Core.StructuralPatterns.Flyweight
+{
+    public class User
+    {
+        public string FullName { get; }
+
+        public User(string fullName)
+        {
+            FullName = fullName;
+        }
+    }
+
+    public class User2
+    {
+        static readonly List<string> Strings = new List<string>();
+        private readonly int[] _names;
+
+        public User2(string fullName)
+        {
+            int GetOrAdd(string s)
+            {
+                var idx = Strings.IndexOf(s);
+                if (idx != -1)
+                    return idx;
+
+                Strings.Add(s);
+                return Strings.Count - 1;
+            }
+
+            _names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+        }
+
+        public string FullName => string.Join(" ", _names.Select(i => Strings[i]));
+    }
+
+   // [TestFixture]
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void ForceGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        public static string RandomString()
+        {
+            Random rand = new Random();
+            return new string(
+              Enumerable.Range(0, 10).Select(i => (char)('a' + rand.Next(26))).ToArray());
+        }
+
+        //[Test]
+        public void TestUser()
+        {
+            var users = new List<User>();
+
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User($"{firstName} {lastName}"));
+
+            ForceGC();
+
+            // dotMemory.Check(memory =>
+            // {
+            //     WriteLine(memory.SizeInBytes);
+            // });
+        }
+
+        //[Test]
+        public void TestUser2()
+        {
+            var users = new List<User2>();
+
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User2($"{firstName} {lastName}"));
+
+            ForceGC();
+
+            // dotMemory.Check(memory =>
+            // {
+            //     WriteLine(memory.SizeInBytes);
+            // });
+        }
+    }
+}
---
+++ DesignPatterns.Core/StructuralPatterns/Flyweight/FlyweightTextFormatting.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Core.StructuralPatterns.Flyweight
+{
+    public class FormattedText
+    {
+        private readonly string _plainText;
+
+        public FormattedText(string plainText)
+        {
+            this._plainText = plainText;
+            _capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int start, int end)
+        {
+            for (int i = start; i <= end; ++i)
+                _capitalize[i] = true;
+        }
+
+        private readonly bool[] _capitalize;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                sb.Append(_capitalize[i] ? char.ToUpper(c) : c);
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class BetterFormattedText
+    {
+        private readonly string _plainText;
+        private readonly List<TextRange> _formatting
+          = new List<TextRange>();
+
+        public BetterFormattedText(string plainText)
+        {
+            this._plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            _formatting.Add(range);
+            return range;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                foreach (var range in _formatting)
+                    if (range.Covers(i) && range.Capitalize)
+                        c = char.ToUpperInvariant(c);
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+    }
+
+    public class FlyweightTextFormattingDemo
+    {
+        static void Main(string[] args)
+        {
+            var ft = new FormattedText("This is a brave new world");
+            ft.Capitalize(10, 15);
+            Console.WriteLine(ft);
+
+            var bft = new BetterFormattedText("This is a brave new world");
+            bft.GetRange(10, 15).Capitalize = true;
+            Console.WriteLine(bft);
+        }
+    }
+}
