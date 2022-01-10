b6994fa03c90b07dff41904762e074209ddf277f
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2022-01-10 22:48:41 +0300

    Command

---
+++ DesignPatterns.Core/BehavioralPatterns/Command/Command.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Core.BehavioralPatterns.Command
+{
+    public class BankAccount
+    {
+        private int _balance;
+        private int overdraftLimit = -500;
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"Deposited ${amount}, balance is now {_balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (_balance - amount >= overdraftLimit)
+            {
+                _balance -= amount;
+                Console.WriteLine($"Withdrew ${amount}, balance is now {_balance}");
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_balance)}: {_balance}";
+        }
+    }
+
+    public interface ICommand
+    {
+        void Call();
+        void Undo();
+    }
+
+    public class BankAccountCommand : ICommand
+    {
+        private readonly BankAccount _account;
+
+        public enum Action
+        {
+            Deposit, Withdraw
+        }
+
+        private readonly Action _action;
+        private readonly int _amount;
+        private bool _succeeded;
+
+        public BankAccountCommand(BankAccount account, Action action, int amount)
+        {
+            _account = account;
+            _action = action;
+            _amount = amount;
+        }
+
+        public void Call()
+        {
+            switch (_action)
+            {
+                case Action.Deposit:
+                    _account.Deposit(_amount);
+                    _succeeded = true;
+                    break;
+                case Action.Withdraw:
+                    _succeeded = _account.Withdraw(_amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Undo()
+        {
+            if (!_succeeded) return;
+            switch (_action)
+            {
+                case Action.Deposit:
+                    _account.Withdraw(_amount);
+                    break;
+                case Action.Withdraw:
+                    _account.Deposit(_amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    class CompositeBankAccountCommand : List<BankAccountCommand>, ICommand
+    {
+        public void Call()
+        {
+            ForEach(cmd => cmd.Call());
+        }
+
+        public void Undo()
+        {
+            foreach (var cmd in
+              ((IEnumerable<BankAccountCommand>)this).Reverse())
+            {
+                cmd.Undo();
+            }
+        }
+    }
+
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount();
+            var cmd = new BankAccountCommand(ba,
+              BankAccountCommand.Action.Deposit, 100);
+            cmd.Call();
+            Console.WriteLine(ba);
+
+            var commands = new List<BankAccountCommand>
+            {
+                new BankAccountCommand(ba, BankAccountCommand.Action.Deposit, 100),
+                new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 1000)
+            };
+
+            Console.WriteLine(ba);
+
+            foreach (var c in commands)
+                c.Call();
+
+            Console.WriteLine(ba);
+
+            foreach (var c in Enumerable.Reverse(commands))
+                c.Undo();
+
+            Console.WriteLine(ba);
+        }
+    }
+}
---
+++ DesignPatterns.Core/BehavioralPatterns/Command/CompositeCommand/CompositeCommand.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Core.BehavioralPatterns.Command.CompositeCommand
+{
+    public class BankAccount
+    {
+        private int _balance;
+        private readonly int _overdraftLimit = -500;
+
+        public BankAccount(int balance = 0)
+        {
+            this._balance = balance;
+        }
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"Deposited ${amount}, balance is now {_balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (_balance - amount >= _overdraftLimit)
+            {
+                _balance -= amount;
+                Console.WriteLine($"Withdrew ${amount}, balance is now {_balance}");
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_balance)}: {_balance}";
+        }
+    }
+
+    public interface ICommand
+    {
+        void Call();
+
+        void Undo();
+
+        bool Success { get; set; }
+    }
+
+    public class BankAccountCommand : ICommand
+    {
+        private readonly BankAccount _account;
+
+        public enum Action
+        {
+            Deposit, Withdraw
+        }
+
+        private readonly Action _action;
+        private readonly int _amount;
+
+        public BankAccountCommand(BankAccount account, Action action, int amount)
+        {
+            this._account = account;
+            this._action = action;
+            this._amount = amount;
+        }
+
+        public void Call()
+        {
+            switch (_action)
+            {
+                case Action.Deposit:
+                    _account.Deposit(_amount);
+                    Success = true;
+                    break;
+                case Action.Withdraw:
+                    Success = _account.Withdraw(_amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Undo()
+        {
+            if (!Success) return;
+            switch (_action)
+            {
+                case Action.Deposit:
+                    _account.Withdraw(_amount);
+                    break;
+                case Action.Withdraw:
+                    _account.Deposit(_amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public bool Success { get; set; }
+    }
+
+    public class CompositeBankAccountCommand
+      : List<BankAccountCommand>, ICommand
+    {
+        public CompositeBankAccountCommand()
+        {
+        }
+
+        public CompositeBankAccountCommand(IEnumerable<BankAccountCommand> collection) : base(collection)
+        {
+        }
+
+        public virtual void Call()
+        {
+            Success = true;
+            ForEach(cmd =>
+            {
+                cmd.Call();
+                Success &= cmd.Success;
+            });
+        }
+
+        public virtual void Undo()
+        {
+            foreach (var cmd in
+              ((IEnumerable<BankAccountCommand>)this).Reverse())
+            {
+                cmd.Undo();
+            }
+        }
+
+        public bool Success { get; set; }
+    }
+
+    public class MoneyTransferCommand : CompositeBankAccountCommand
+    {
+        public MoneyTransferCommand(BankAccount from,
+          BankAccount to, int amount)
+        {
+            AddRange(new[]
+            {
+                new BankAccountCommand(from, BankAccountCommand.Action.Withdraw, amount),
+                new BankAccountCommand(to, BankAccountCommand.Action.Deposit, amount),
+            });
+        }
+
+        public override void Call()
+        {
+            BankAccountCommand last = null;
+            foreach (var cmd in this)
+            {
+                if (last == null || last.Success)
+                {
+                    cmd.Call();
+                    last = cmd;
+                }
+                else
+                {
+                    cmd.Undo();
+                    break;
+                }
+            }
+        }
+    }
+
+    class CompositeCommandDemo
+    {
+        static void Main(string[] args)
+        {
+            // composite
+            var ba = new BankAccount();
+            var cmdDeposit = new BankAccountCommand(ba, BankAccountCommand.Action.Deposit, 100);
+            var cmdWithdraw = new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 1000);
+            var composite = new CompositeBankAccountCommand(new[]{ cmdDeposit, cmdWithdraw });
+
+            composite.Call();
+            Console.WriteLine(ba);
+
+            composite.Undo();
+            Console.WriteLine(ba);
+
+
+            // money transfer
+            var from = new BankAccount();
+            from.Deposit(100);
+            var to = new BankAccount();
+
+            var mtc = new MoneyTransferCommand(from, to, 1000);
+            mtc.Call();
+
+            Console.WriteLine(from);
+            Console.WriteLine(to);
+
+            mtc.Undo();
+
+            Console.WriteLine(from);
+            Console.WriteLine(to);
+        }
+    }
+}
