af20f394f2d0f3d4f947d506e873d4419e99bc76
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2022-01-11 23:29:06 +0300

    Mediator

---
+++ DesignPatterns.Core/BehavioralPatterns/Mediator/MediatorWithEvents.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace DesignPatterns.Core.BehavioralPatterns.Mediator
+{
+    abstract class GameEventArgs : EventArgs
+    {
+        public abstract void Print();
+    }
+
+    class PlayerScoredEventArgs : GameEventArgs
+    {
+        public string PlayerName;
+        public int GoalsScoredSoFar;
+
+        public PlayerScoredEventArgs
+            (string playerName, int goalsScoredSoFar)
+        {
+            PlayerName = playerName;
+            GoalsScoredSoFar = goalsScoredSoFar;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine($"{PlayerName} has scored! " +
+                      $"(their {GoalsScoredSoFar} goal)");
+        }
+    }
+
+    class Game
+    {
+        public event EventHandler<GameEventArgs> Events;
+
+        public void Fire(GameEventArgs args)
+        {
+            Events?.Invoke(this, args);
+        }
+    }
+
+    class Player
+    {
+        private string name;
+        private int goalsScored = 0;
+        private Game game;
+
+        public Player(string name, Game game)
+        {
+            this.name = name;
+            this.game = game;
+        }
+
+        public void Score()
+        {
+            goalsScored++;
+            var args = new PlayerScoredEventArgs(name, goalsScored);
+            game.Fire(args);
+        }
+    }
+
+    class Coach
+    {
+        private Game game;
+
+        public Coach(Game game)
+        {
+            this.game = game;
+
+            // celebrate if player has scored <3 goals
+            game.Events += (sender, args) =>
+            {
+                if (args is PlayerScoredEventArgs scored
+                    && scored.GoalsScoredSoFar < 3)
+                {
+                    Console.WriteLine($"coach says: well done, {scored.PlayerName}");
+                }
+            };
+        }
+    }
+
+    class MediatorWithEventsDemo
+    {
+        public static void Main(string[] args)
+        {
+            var game = new Game();
+            var player = new Player("Sam", game);
+            var coach = new Coach(game);
+
+            player.Score(); // coach says: well done, Sam
+            player.Score(); // coach says: well done, Sam
+            player.Score(); // ignored by coach
+        }
+    }
+}
---
+++ DesignPatterns.Core/BehavioralPatterns/Mediator/MediatorWithRx/MediatorWithRx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Core.BehavioralPatterns.Mediator.MediatorWithRx
+{
+    /*[UsedImplicitly]*/
+    public class EventBroker : IObservable<EventArgs>
+    {
+        private readonly List<Subscription> _subscribers = new List<Subscription>();
+
+        public IDisposable Subscribe(IObserver<EventArgs> subscriber)
+        {
+            var sub = new Subscription(this, subscriber);
+            if (_subscribers.All(s => s.Subscriber != subscriber))
+                _subscribers.Add(sub);
+            return sub;
+        }
+
+        private void Unsubscribe(IObserver<EventArgs> subscriber)
+        {
+            _subscribers.RemoveAll(s => s.Subscriber == subscriber);
+        }
+
+        public void Publish<T>(T args) where T : EventArgs
+        {
+            foreach (var s in _subscribers.ToArray())
+                s.Subscriber.OnNext(args); // will call Unsubscribe() from here
+        }
+
+        private class Subscription : IDisposable
+        {
+            private readonly EventBroker broker;
+            public IObserver<EventArgs> Subscriber { get; private set; }
+            public Subscription(EventBroker broker, IObserver<EventArgs> subscriber)
+            {
+                this.broker = broker;
+                Subscriber = subscriber;
+            }
+            public void Dispose()
+            {
+                broker.Unsubscribe(Subscriber);
+            }
+        }
+    }
+
+    class PlayerScoredEventArgs : EventArgs
+    {
+        public string PlayerName;
+        public int GoalsScoredSoFar;
+
+        public PlayerScoredEventArgs
+          (string playerName, int goalsScoredSoFar)
+        {
+            PlayerName = playerName;
+            GoalsScoredSoFar = goalsScoredSoFar;
+        }
+    }
+
+    /*[UsedImplicitly]*/
+    public class Player
+    {
+        public string Name;
+        private int goalsScored;
+        private EventBroker broker;
+
+        public delegate Player Factory(string name);
+
+        public Player(string name, EventBroker broker)
+        {
+            Name = name;
+            this.broker = broker;
+        }
+
+        public void Score()
+        {
+            goalsScored++;
+            var args = new PlayerScoredEventArgs(Name, goalsScored);
+            broker.Publish(args);
+        }
+    }
+
+    /*[UsedImplicitly]*/
+    public class Coach
+    {
+        private IDisposable subscription;
+
+        public Coach(EventBroker broker)
+        {
+            /*subscription = broker
+              .OfType<PlayerScoredEventArgs>()
+              .Skip(1)
+              .Take(3)
+              .Subscribe(args =>
+                Console.WriteLine($"Well done, {args.PlayerName}! ({args.GoalsScoredSoFar} goals)"));*/
+        }
+    }
+
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            var cb = new ContainerBuilder();
+            cb.RegisterType<EventBroker>().SingleInstance();
+            cb.RegisterType<Player>();
+            cb.RegisterType<Coach>();
+
+            var container = cb.Build();
+
+            var pf = container.Resolve<Player.Factory>();
+            var player = pf("John");
+
+            var coach = container.Resolve<Coach>();
+
+            player.Score();
+            player.Score(); //
+            player.Score(); //
+            player.Score(); //
+            player.Score();
+            */
+        }
+    }
+}
