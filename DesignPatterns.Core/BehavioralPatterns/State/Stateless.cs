604c12f63a10795e4b34238fc857702f246afcfb
Nikita Kuzmin <kuzmin.nick@rambler.ru>
2022-01-13 00:11:19 +0300

    Observer, State, Strategy

---
+++ DesignPatterns.Core/BehavioralPatterns/State/Stateless.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*using Stateless;*/
+
+namespace DesignPatterns.Core.BehavioralPatterns.State
+{
+    class StatelessProgram
+    {
+        enum Trigger
+        {
+            On, Off
+        }
+
+        public static void Main(string[] args)
+        {
+            // false = off, true = on
+
+            /*var light = new StateMachine<bool, Trigger>(false);
+
+            light.Configure(false) // if the light is off...
+                .Permit(Trigger.On, true)  // we can turn it on
+                .OnEntry(transition =>
+                {
+                    if (transition.IsReentry)
+                        WriteLine("Light is already off!");
+                    else
+                        WriteLine("Switching light off");
+                })
+                .PermitReentry(Trigger.Off);
+            // .Ignore(Trigger.Off) // but if it's already off we do nothing
+
+            // same for when the light is on
+            light.Configure(true)
+                .Permit(Trigger.Off, false)
+                .OnEntry(() => WriteLine("Turning light on"))
+                .Ignore(Trigger.On);
+
+            light.Fire(Trigger.On);  // Turning light on
+            light.Fire(Trigger.Off); // Turning light off
+            light.Fire(Trigger.Off); // Light is already off!
+            */
+        }
+    }
+}
---
+++ DesignPatterns.Core/BehavioralPatterns/Strategy/StrategyStatic/StrategyStatic.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Core.BehavioralPatterns.Strategy.StrategyStatic
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            // markdown doesn't require a list preamble
+        }
+
+        public void End(StringBuilder sb)
+        {
+
+        }
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"  <li>{item}</li>");
+        }
+    }
+
+    // a.k.a. policy
+    public class TextProcessor<LS>
+        where LS : IListStrategy, new()
+    {
+        private readonly StringBuilder _sb = new StringBuilder();
+        private readonly IListStrategy _listStrategy = new LS();
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            _listStrategy.Start(_sb);
+            foreach (var item in items)
+                _listStrategy.AddListItem(_sb, item);
+            _listStrategy.End(_sb);
+        }
+
+        public override string ToString()
+        {
+            return _sb.ToString();
+        }
+    }
+
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessor<MarkdownListStrategy>();
+            tp.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp);
+
+            var tp2 = new TextProcessor<HtmlListStrategy>();
+            tp2.AppendList(new[] { "foo", "bar", "baz" });
+            Console.WriteLine(tp2);
+        }
+    }
+}
